12cda1b5fd322bdac154f608e48ae00a2bd9db78
wladus <wladusdrem@gmail.com>
2024-09-24 12:51:08 +0300

    Fix

--- src/app/(root)/users/page.tsx
+++ src/app/(root)/users/page.tsx
@@ -2,14 +2,14 @@ import Loading from '@/app/loading-component'
 import { UsersList } from '@/components'
 import { Suspense } from 'react'
 
-export default async function Users() {
+export default function Users() {
 	return (
 		<>
 			<h1 className='text-center mb-3'>Users</h1>
 			<div className='relative w-full h-[600px] overflow-y-auto px-3'>
-				<Suspense fallback={<Loading />}>
+				{/* <Suspense fallback={<Loading />}>
 					<UsersList />
-				</Suspense>
+				</Suspense> */}
 			</div>
 		</>
 	)
