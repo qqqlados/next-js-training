f50b957b4d7ac0c57cd523dd4e0abdfc66c5bd0e
wladus <wladusdrem@gmail.com>
2024-10-19 18:18:05 +0300

    deleted some prisma models

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,4 +1,4 @@
 export { addUser } from './actions'
-export { useCheckDatabase } from './use-check-database'
+export { useCheckLogin, useCheckRegister } from './use-check-database'
 export { useValidateUserData } from './use-validate-user-data'
 export { useNetworkFormError } from './use-network-form-error'
