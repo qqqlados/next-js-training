9f24316d09a72d77b346cdbc6349c9a1468fac80
wladus <wladusdrem@gmail.com>
2024-10-19 22:46:04 +0300

    fixed build bugs

--- src/app/api/albums/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from 'next/server'
-import { prisma } from '../../../../prisma/prisma-client'
-
-export async function GET() {
-	const albums = await prisma.album.findMany()
-
-	return NextResponse.json(albums)
-}
